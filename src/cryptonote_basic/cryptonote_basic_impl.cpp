22d2c276585bac011a344f7718d9e5fd72b7abd0
gr0wth.eth.xmr <38635290+ethgr0wth@users.noreply.github.com>
2024-02-10 19:39:37 -0500

    Update cryptonote_basic_impl.cpp

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -84,15 +84,16 @@ namespace cryptonote {
     static_assert(DIFFICULTY_TARGET_V2%60==0&&DIFFICULTY_TARGET_V1%60==0,"difficulty targets must be a multiple of 60");
     const int target = version < 2 ? DIFFICULTY_TARGET_V1 : DIFFICULTY_TARGET_V2;
     const int target_minutes = target / 60;
-    const int emission_speed_factor = height <= 1 ? EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1) : EMISSION_SPEED_FACTOR_PER_MINUTE + 6;
+    const int emission_speed_factor = height <= 1 ? EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1) : EMISSION_SPEED_FACTOR_PER_MINUTE + 5;
     
     const uint64_t genesis = 2000000000000000000U;
     if ((uint64_t)height == 1) {
       reward = genesis;
       return true;
     }
-    
-    uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+    uint64_t base_reward_v1 = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+    uint64_t base_reward_v2 = (COIN_SUPPLY - already_generated_coins) >> emission_speed_factor;
+    uint64_t base_reward = height < 1 ? base_reward_v1 : base_reward_v2;
     if (base_reward < FINAL_SUBSIDY_PER_MINUTE*target_minutes)
     {
       base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
